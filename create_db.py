cb5593a700aa69430516d27f3cc021c36bc985bc
Rodrigo Lopes de Carvalho <rodrigo@rodrigo.lc>
2016-04-19 16:43:23 -0300

    adicionadas tabelas ao script de criação

--- create_db.py
+++ create_db.py
@@ -9,6 +9,8 @@ cur = db.cursor()
 
 #criação de tabelas
 cur.execute('''create table Time (url_suffix text primary key, nome text)''')
-
+cur.execute('''create table Jogador (url_suffix text primary key, apelido text, nome text, sobrenome text)''')
+cur.execute('''create table Noticia (url_suffix text primary key, titulo text,subtitulo text)''')
+cur.execute('''create table JogadorNoTime (url_suffix_time text, url_suffix_jogador text)''')
 
 db.commit()
